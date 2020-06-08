9ceeaac21c7fd51ebb80a589feffd0a31347e614
Phil DeLong <philjdelong@gmail.com>
2020-06-08 09:52:37 -0600

    Create philDeLong object and functionality

---
+++ 2-JS-basics/starter/functions.js
@@ -0,0 +1,18 @@
+function ageCalculator(birthYear) {
+	return 2020 - birthYear;
+}
+
+var philAge = ageCalculator(1990);
+console.log(philAge);
+
+// function retirementYear(ageCalculator(birthYear)) {
+
+// }
+
+var philDeLong = {
+	name: "Phil DeLong",
+	age: 30,
+	location: ["denver", "co"]
+};
+
+console.log(philDeLong.location[0], philDeLong.location[1]);
